a847e45df24890ec400c3e8862adc10c2bff32f9
proXDhiya <prox.dhiya@gmail.com>
2022-11-25 12:06:19 +0100

    Complete add new cow backend

--- controllers/register-cow/post-register-cow.js
+++ controllers/register-cow/post-register-cow.js
@@ -5,6 +5,15 @@ module.exports = (req, res) => {
         return res.redirect('/dashboard');
     
     // Get the data from the request body
-    // Null for now
+    const { name, birthday, color, breed, motherId } = req.body;
+
+    // add cow to database
+    if (!require('../../modules/module_cows').setNewCow(name, birthday, color, breed, motherId)) {
+        res.session.error = 'Error adding cow to database';
+        return res.redirect('/register-cow');
+    }
+
+    // Redirect to dashboard
+    req.session.success = 'Cow added successfully';
     return res.redirect('/dashboard');
 };
---
+++ modules/module_cows.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.setNewCow = (name, birthday, color, breed, motherId) => {
+    // Read File
+    const cows = fs.readFileSync(path.join(__dirname, '../database/cows.json'), 'utf8');
+
+    // Parse JSON
+    const cowsJSON = JSON.parse(cows);
+
+    // check if mother cow exists
+    if (motherId !== '') {
+        const motherCow = cowsJSON.find(cow => cow.id === motherId);
+        // if mother cow does not exist, return error
+        if (!motherCow) {
+            console.log('Mother cow does not exist');
+            return false;
+        }
+    }
+    
+    // Create new cow
+    const newCow = {
+        id: (cowsJSON.length + 1).toString(),
+        "cow-name": name,
+        "birth-day": birthday,
+        "color": color,
+        "breed": breed,
+        "mother-id": motherId === '' ? null : motherId,
+    };
+
+    // Push new cow to cowsJSON
+    cowsJSON.push(newCow);
+
+    // Write to file
+    try {
+        fs.writeFileSync(path.join(__dirname, '../database/cows.json'), JSON.stringify(cowsJSON, null, 4));
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+};
