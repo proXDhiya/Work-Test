8afd06c0dd2a15316f05c6c07be570932d28f7f8
proXDhiya <prox.dhiya@gmail.com>
2022-11-26 18:19:19 +0100

    Complete router register-new-birth

--- routers/register-new-birth.js
+++ routers/register-new-birth.js
@@ -6,5 +6,8 @@ const router = express.Router();
 // GET request for register-new-birth
 router.get('/', require('../controllers/register-new-birth/GET_register-new-birth'));
 
+// POST request for register-new-birth
+router.post('/', require('../controllers/register-new-birth/POST_register-new-birth'));
+
 // export the router
 module.exports = router;
