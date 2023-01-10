14b7f70e8bb6b5e30afc11b2db8f77ba8bd60024
proXDhiya <prox.dhiya@gmail.com>
2023-01-10 05:33:14 +0100

    Add Opentelemetry & jaeger

--- tracing.js
+++ tracing.js
@@ -1,31 +1,44 @@
-const process = require('process');
 const opentelemetry = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 
-// configure the SDK to export telemetry data to the console
-// enable all auto-instrumentations from the meta package
-const traceExporter = new ConsoleSpanExporter();
+const zipkinExporter = new ZipkinExporter({
+  serviceName: 'my-service',
+  url: 'http://localhost:9411/api/v2/spans',
+});
+
+const jaegerExporter = new JaegerExporter({
+  serviceName: 'my-service',
+  host: 'localhost',
+  port: 6832,
+});
+
 const sdk = new opentelemetry.NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'my-service',
   }),
-  traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()]
+  traceExporter: jaegerExporter,
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-http': {
+        enabled: true,
+        applyCustomAttributesOnSpan: (span, request) => {
+          span.setAttribute('http.request.headers', request.headers);
+          span.setAttribute('http.request.method', request.method);
+          span.setAttribute('http.request.url', request.url);
+        }
+      }
+    })
+  ]
 });
 
-// initialize the SDK and register with the OpenTelemetry API
-// this enables the API to record telemetry
-sdk.start()
-  .then(() => console.log('Tracing initialized'))
-  .catch((error) => console.log('Error initializing tracing', error));
-
-// gracefully shut down the SDK on process exit
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('Tracing terminated'))
-    .catch((error) => console.log('Error terminating tracing', error))
-    .finally(() => process.exit(0));
+sdk.start().then(() => {
+  console.log('tracing initialized');
 });
+
+// sudo docker run -d -p 5775:5775/udp   -p 6831:6831/udp   
+// -p 6832:6832/udp   -p 5778:5778   -p 16686:16686   -p 14250:14250   
+// -p 14268:14268   -p 14269:14269   -p 9411:9411   jaegertracing/all-in-one
